3e3e2de6180f28fd475a34dceca1a28aadf5afd8
zjellena <152310518+zjellena@users.noreply.github.com>
2024-09-24 19:10:17 +0200

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -14,6 +14,7 @@ class TestHootel(object):
         options.add_argument('--headless')
         self.browser = webdriver.Chrome(options=options)
         self.browser.get(URL)
+        self.browser.set_window_size(1024, 768)
 
     def teardown_method(self):
         self.browser.quit()
